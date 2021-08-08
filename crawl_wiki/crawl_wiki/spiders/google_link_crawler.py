cbe1527a4cbd6336e2fe597362d7e23d85a85490
Tuyên Đàm Trọng <35034919+trongtuyen99@users.noreply.github.com>
2021-08-08 08:59:01 +0700

    update for bing crawler

--- crawl_wiki/crawl_wiki/spiders/google_link_crawler.py
+++ crawl_wiki/crawl_wiki/spiders/google_link_crawler.py
@@ -2,6 +2,7 @@ import scrapy
 from bs4 import BeautifulSoup
 import urllib
 from scrapy.linkextractors import LinkExtractor
+custom_user_agent = "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0"
 google_domains = ('https://www.google.', 
                 'https://google.', 
                 'https://webcache.googleusercontent.', 
@@ -9,7 +10,13 @@ google_domains = ('https://www.google.',
                 'https://policies.google.',
                 'https://support.google.',
                 'https://maps.google.',
-                'https://accounts.google.com')
+                'https://accounts.google.com',
+                'http://go.microsoft.',
+                'https://go.microsoft.',
+                'http://www.microsofttranslator.com',
+                'https://www.youtube.com',
+                'https://www.facebook.com',
+                'http://help.bing.')
 class GoogleLinkSpider(scrapy.Spider):
     name = "google_link"
 
@@ -18,26 +25,33 @@ class GoogleLinkSpider(scrapy.Spider):
         with open('google_queries.txt', 'r') as file:
             urls = file.readlines()
         for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+            url = url.replace('.google.', '.bing.')
+            yield scrapy.Request(url=url, callback=self.parse,\
+             headers={"User-Agent": custom_user_agent})
 
     def parse(self, response):
         url = response.url
-        all_links = []
+        all_links = set()
         crawled_source = set()
         try:
             html_parse = BeautifulSoup(response.body, 'html.parser')
             
             for para in html_parse.findAll('a', href=True):
-                link = para.get('href')
-                if link.startswith('/url?q=https'):
-                    source = link.split('/')[3]
-                    if source not in crawled_source:
-                        crawled_source.add(source)
-                        use_link = link[7:].split('&')[0]
-                        if not use_link.startswith(google_domains) and 'wikipedia' not in use_link:
-                            all_links.append(use_link)
+                try:
+                    link = para.get('href')
+                    if link.startswith('http') and not link.startswith(google_domains) and 'wikipedia' not in link:
+                        all_links.add(link)
+                    # if link.startswith('/url?q=https'):
+                    #     source = link.split('/')[3]
+                    #     if source not in crawled_source:
+                    #         crawled_source.add(source)
+                    #         use_link = link[7:].split('&')[0]
+                    #         if not use_link.startswith(google_domains) and 'wikipedia' not in use_link:
+                    #             all_links.append(use_link)
+                except:
+                    pass
 
         except Exception as e:
             print(f"Get content wiki {url} exception: {e}")
         
-        yield {url: all_links}
+        yield {url: list(all_links)}
